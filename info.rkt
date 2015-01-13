61dc9f9776c5fc1215d74b12d84343d91329da7b
Asumu Takikawa <asumu@ccs.neu.edu>
2015-01-13 18:49:53 -0500

    Add at-exp-lib dependency

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "classicthesis")
 
-(define deps '("base" "scribble-lib"))
+(define deps '("base" "scribble-lib" "at-exp-lib"))
 
 (define pkg-desc "Port of the classicthesis style to Scribble")
 (define pkg-authors '(stamourv))
